0706a4433e85442f444d14343ab0d1aeac9d7a95
Victor Owiti <viowiti12@gmail.com>
2022-10-27 22:03:27 +0300

    initial commit

---
+++ App.js
@@ -0,0 +1,13 @@
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator, FlatList, Text, View } from "react-native";
+
+export default function App() {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+
+  return (
+    <View style={{ flex: 1, padding: 24 }}>
+      <Text>We up!</Text>
+    </View>
+  );
+}

edc4a2d068bedaf636cfac95800ef4471624b267
Victor Owiti <viowiti12@gmail.com>
2022-10-28 13:33:01 +0300

    Added the data fetching functionality

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import React, { useEffect, useState } from "react";
 import { ActivityIndicator, FlatList, Text, View } from "react-native";
+import { useApi } from "./hooks/useApi";
 
 export default function App() {
   const [isLoading, setLoading] = useState(true);
   const [data, setData] = useState([]);
+  const { getBooks } = useApi();
 
+  useEffect(() => {
+    let bookData = getBooks();
+    setData(bookData);
+    setLoading(false);
+  }, []);
   return (
     <View style={{ flex: 1, padding: 24 }}>
       <Text>We up!</Text>
---
+++ hooks/useApi.js
@@ -0,0 +1,11 @@
+export const useApi = () => {
+  const getBooks = async () => {
+    const data = await fetch("https://fudap-books-api.herokuapp.com/books/");
+
+    let jsonData = await data.json();
+
+    return jsonData;
+  };
+
+  return { getBooks };
+};

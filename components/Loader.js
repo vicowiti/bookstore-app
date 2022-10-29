79e7cbc48419380a891e2e52d69ebcc1903ee690
Victor Owiti <viowiti12@gmail.com>
2022-10-30 00:06:54 +0300

    Tidying up

--- components/Loader.js
+++ components/Loader.js
@@ -8,12 +8,21 @@ export default function Loader() {
         width: "100%",
         height: "100%",
         flex: 1,
+
         justifyContent: "center",
         zIndex: 100,
       }}
     >
-      <ActivityIndicator size={`large`} color="#111" />
-      <Text>Loading ...</Text>
+      <View
+        style={{
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
+        }}
+      >
+        <ActivityIndicator size={`large`} color="#111" />
+        <Text>Loading ...</Text>
+      </View>
     </View>
   );
 }
